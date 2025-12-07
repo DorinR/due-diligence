4f6b5e6e6207ddd1368c58df57186f16b905f796
dorin <dorin.rogov@gmail.com>
2025-12-07 17:08:06 -0500

    Rename

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -26,7 +26,7 @@ export const Navbar = () => {
             <div className="mx-auto px-8">
                 <div className="flex items-center justify-between py-4">
                     <Link to="/" className="flex items-center space-x-4 no-underline">
-                        <div className="font-title text-2xl">semantica</div>
+                        <div className="font-title text-2xl">Fincite</div>
                     </Link>
                     <div className="flex items-center space-x-4">
                         {isAuthenticated ? (
